bb3300592f56d38d2babbf33afa6c90b4fa1d9cc
ansonann0407 <ansonannwk@gmail.com>
2024-05-06 16:58:47 +0800

    up 登入畫面和地圖整合 5/6

--- MyBicycle 2/app/src/main/java/com/example/mybicycle/MainActivity.java
+++ MyBicycle 2/app/src/main/java/com/example/mybicycle/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.mybicycle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, map.class); // Assuming MapActivity.class represents your map functionality
+                    startActivity(intent);
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
---
+++ MyBicycle 2/app/src/main/java/com/example/mybicycle/map.java
@@ -0,0 +1,13 @@
+package com.example.mybicycle; // Assuming this is your package name
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map); // Replace "map" with the actual name of your map.xml file
+    }
+}
