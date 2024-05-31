1bd8106b70f924821c53ad205f291b3b83fe97f4
dudago2 <a11223010@gemail.yuntech.edu.tw>
2024-05-31 16:46:12 +0800

    database connection

--- MyBicycle 2/app/build.gradle.kts
+++ MyBicycle 2/app/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    // https://mvnrepository.com/artifact/mysql/mysql-connector-java
+    implementation("mysql:mysql-connector-java:5.1.49")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ MyBicycle 2/app/src/main/java/com/example/mybicycle/ConnectionClass.java
@@ -0,0 +1,2 @@
+package com.example.mybicycle;public class ConnectionClass {
+}
